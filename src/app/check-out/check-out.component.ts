0fd9232f2342c21484f8267f4ecf2655b056c04a
Prakash Pun <prakash_pun@persistent.co.in>
2019-04-09 15:41:58 +0530

    Implementing check out orders page

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCartService } from '../shopping-cart.service';
+import { ShoppingCart } from 'src/models/shopping-cart';
+import { Subscription } from 'rxjs';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  shipping = {};
+  cart: ShoppingCart;
+  subscription: Subscription;
 
-  ngOnInit() {
+  constructor(private orderService: OrderService,
+              private shoppingCartService: ShoppingCartService) {}
+
+  async ngOnInit() {
+    const cart$ = await this.shoppingCartService.getCart();
+    this.subscription = cart$.subscribe(cart => this.cart = cart );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  placeOrder() {
+    const order = {
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.cart.items.map(i => {
+        return {
+          product: {
+            title: i.product.title,
+            imageUrl: i.product.imageUrl,
+            price: i.product.price
+          },
+          quantity: i.quantity,
+          totalPrice: i.totalPrice
+        };
+      })
+    };
+
+    this.orderService.storeOrder(order);
   }
 
 }
